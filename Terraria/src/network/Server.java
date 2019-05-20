95144b553fdcbad626ea6abf6f6dc9462b2e50c6
Aiden Zhang <aidencz8@gmail.com>
2019-05-20 14:57:21 -0700

    Networking Basics, Unfinished

--- Terraria/src/network/Server.java
+++ Terraria/src/network/Server.java
@@ -1,5 +1,37 @@
 package network;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
+	public static final int port = 25565;
+	private ServerSocket server;
+	private Socket client;
+	
+	public ServerSocket getServer() {
+		return server;
+	}
+	
+	public Socket getClient() {
+		return client;
+	}
+	
+	public void listen() {
+		try {
+			server = new ServerSocket(25565);
+		} catch (Exception e) {
+			System.out.println("Listening failed on Port " + port);
+			System.exit(-1);
+			//e.printStackTrace();
+		}
+		
+		try {
+			client = server.accept();
+		} catch (Exception e) {
+			System.out.println("Client could not be initialized on " + port);
+			System.exit(-1);
+		}
+	}
+	
 }
