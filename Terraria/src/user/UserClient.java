4388238b3fd2911819d35c376f87a00135ed3e47
Aiden Zhang <aidencz8@gmail.com>
2019-05-21 08:29:19 -0700

    Finished WORKING Chat System
    Yay!

--- Terraria/src/user/UserClient.java
+++ Terraria/src/user/UserClient.java
@@ -5,9 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
-public class UserClient {
+public class UserClient implements Runnable {
 
 	public static void main(String[] args) throws IOException {
 		Scanner keyboard = new Scanner(System.in);
@@ -20,19 +21,36 @@ public class UserClient {
 			return;
 		}
 		Socket socket = new Socket(hostname, 4321);
-		Scanner input = new Scanner(socket.getInputStream());
-		System.out.println("Server: " + input.nextLine());
+		//Scanner input = new Scanner(socket.getInputStream());
+		//System.out.println("Server: " + input.nextLine());
 		PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+		//Scanner sockscan = new Scanner(socket.getInputStream());
+		Thread t = new Thread(new UserClient());
+		t.start();
 		while (true) {
-			input = new Scanner(socket.getInputStream());
+			//BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			if (keyboard.hasNextLine()) {
 				output.println(name + ": " + keyboard.nextLine());
 			}
 			// fix printing multiples, and print overlapping from multiples issue
-			System.out.println(input.nextLine());
+			/*while (reader.ready()) {
+				System.out.println(sockscan.nextLine());
+			}*/
 		}
 	}
 	
-	
-	
+	@Override
+	public void run() {
+		Socket socket;
+		System.out.println("Runnable Created");
+		try {
+			socket = new Socket("localhost", 4321);
+			while (true) {
+				Scanner sockscan = new Scanner(socket.getInputStream());
+				System.out.println(sockscan.nextLine());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
